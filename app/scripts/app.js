97dbf8652381cfae5abca58a11090bd2041df5bd
Tuyen Nguyen Gia <giatuyentiensinh@gmail.com>
2015-10-27 15:29:10 +0700

    update image and toast, tooltip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-var app = angular
+angular
   .module('amApp', [
     'ngAnimate',
     'ngCookies',
--- app/scripts/router.config.js
+++ app/scripts/router.config.js
@@ -1,5 +1,6 @@
 'use strict';
-app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+angular.module('amApp')
+	.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
           $rootScope.$state = $state;
           $rootScope.$stateParams = $stateParams;
       }]
@@ -10,28 +11,46 @@ app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $
 			.state('app', {
 				abstract: true,
 				url: '/app',
-				templateUrl: 'views/layout.html'
+				templateUrl: 'views/layout.html',
+				controller: function($window) {
+					$window.document.title = 'Tuyen page';
+				}
 			})
 			.state('app.home', {
 				url: '/home',
-				templateUrl: 'views/home.html'
+				templateUrl: 'views/home.html',
+				controller: function($window) {					
+					$window.document.title = 'Tuyen page';
+				}
 			})
 			.state('app.about', {
 				url: '/about',
-				templateUrl: 'views/about.html'
+				templateUrl: 'views/about.html',
+				controller: function($window) {					
+					$window.document.title = 'Tuyen page';
+				}
 			})
 			.state('app.contact', {
 				url: '/contact',
-				templateUrl: 'views/contact.html'
+				templateUrl: 'views/contact.html',
+				controller: function($window) {					
+					$window.document.title = 'Tuyen page';
+				}
 			})
 			.state('app.help', {
 				url: '/help',
-				templateUrl: 'views/help.html'
+				templateUrl: 'views/help.html',
+				controller: function($window) {					
+					$window.document.title = 'xforce team';
+				}
 			})
 			.state('kimanh', {
 				abstract: true,
 				url: '/kimanh',
-				templateUrl: 'views/kimanh/kimanh.html'
+				templateUrl: 'views/kimanh/kimanh.html',
+				controller: function($window) {					
+					$window.document.title = 'Kim Anh page';
+				}
 			})
 			.state('kimanh.tongquan', {
 				url: '/tongquan',
